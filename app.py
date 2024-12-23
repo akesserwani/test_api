2c6cc4b74ee212646518a7878c9fed3ed4208c74
Ali Kesserwani <akesser@uab.edu>
2024-12-23 10:22:59 -0600

    add data

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/message', methods=['GET', 'POST'])
+def message():
+    # Default response data
+    response_data = {
+        "message": "Check out the new update!",
+        "showModal": False
+    }
+
+    # Handle POST requests
+    if request.method == 'POST':
+        data = request.get_json()  # Get JSON data from the request
+        if data and 'message' in data and 'showModal' in data:
+            response_data['message'] = data['message']
+            response_data['showModal'] = bool(data['showModal'])
+
+    # Return the response as JSON
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
